10a386116981575ef50e4bf82bb3c816d0aa8b88
Rannuo <wangrannuo@gmail.com>
2024-01-12 15:28:32 -0500

    add create task feature and input box

--- client/src/components/Checklist.jsx
+++ client/src/components/Checklist.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   MDBBtn,
   MDBCard,
@@ -13,11 +13,15 @@ import {
   MDBTextArea,
   MDBTooltip,
 } from "mdb-react-ui-kit";
+import ChecklistForm from "./ChecklistForm";
+import TaskList from "./TaskList";
 
 function Checklist() {
+    const [tasks, setTasks] = useState(["first", "second", "third", "fourth", "fifth"]);
+ 
   return (
-    <div class="col-md-7">
-      <div class="h-100 p-10 text-bg-dark rounded-3">
+    <div className="col-md-7">
+      <div className="h-100 p-10 text-bg-dark rounded-3">
         <section className="vh-100">
           <MDBContainer className="py-5 h-100">
             <MDBRow className="d-flex justify-content-center align-items-center">
@@ -25,129 +29,12 @@ function Checklist() {
                 <MDBCard style={{ borderRadius: "15px" }}>
                   <MDBCardBody className="p-5">
                     <h6 className="checklist-title mb-3">Todo List</h6>
-                    <div className="d-flex justify-content-center align-items-center mb-4">
-                      <MDBTextArea
-                    
-                        id="textAreaExample"
-                        rows={4}
-                        wrapperClass="flex-fill"
-                      />
-                      <MDBBtn type="submit" size="lg" className="ms-2">
-                        Add
-                      </MDBBtn>
-                    </div>
-                    <MDBListGroup className="mb-0">
-                      <MDBListGroupItem className="d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
-                        <div className="d-flex align-items-center">
-                          <MDBCheckbox
-                            name="flexCheck"
-                            value=""
-                            id="flexCheckChecked"
-                            className="me-3"
-                          />
-                          Task list and assignments
-                        </div>
-                        <MDBTooltip
-                          tag="a"
-                          wrapperProps={{ href: "#!" }}
-                          title="Remove item"
-                        >
-                          <MDBIcon fas icon="times" color="primary" />
-                        </MDBTooltip>
-                      </MDBListGroupItem>
-                      <MDBListGroupItem className="d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
-                        <div className="d-flex align-items-center">
-                          <MDBCheckbox
-                            name="flexCheck"
-                            value=""
-                            id="flexCheckChecked"
-                            className="me-3"
-                            defaultChecked
-                          />
-                          <s>Dapibus ac facilisis in</s>
-                        </div>
-                        <MDBTooltip
-                          tag="a"
-                          wrapperProps={{ href: "#!" }}
-                          title="Remove item"
-                        >
-                          <MDBIcon fas icon="times" color="primary" />
-                        </MDBTooltip>
-                      </MDBListGroupItem>
-                      <MDBListGroupItem className="d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
-                        <div className="d-flex align-items-center">
-                          <MDBCheckbox
-                            name="flexCheck"
-                            value=""
-                            id="flexCheck"
-                            className="me-3"
-                          />
-                          Morbi leo risus
-                        </div>
-                        <MDBTooltip
-                          tag="a"
-                          wrapperProps={{ href: "#!" }}
-                          title="Remove item"
-                        >
-                          <MDBIcon fas icon="times" color="primary" />
-                        </MDBTooltip>
-                      </MDBListGroupItem>
-                      <MDBListGroupItem className="d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
-                        <div className="d-flex align-items-center">
-                          <MDBCheckbox
-                            name="flexCheck"
-                            value=""
-                            id="flexCheck"
-                            className="me-3"
-                          />
-                          Porta ac consectetur ac
-                        </div>
-                        <MDBTooltip
-                          tag="a"
-                          wrapperProps={{ href: "#!" }}
-                          title="Remove item"
-                        >
-                          <MDBIcon fas icon="times" color="primary" />
-                        </MDBTooltip>
-                      </MDBListGroupItem>
-                      <MDBListGroupItem className="d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
-                        <div className="d-flex align-items-center">
-                          <MDBCheckbox
-                            name="flexCheck"
-                            value=""
-                            id="flexCheckChecked"
-                            className="me-3"
-                            defaultChecked
-                          />
-                          Vestibulum at eros
-                        </div>
-                        <MDBTooltip
-                          tag="a"
-                          wrapperProps={{ href: "#!" }}
-                          title="Remove item"
-                        >
-                          <MDBIcon fas icon="times" color="primary" />
-                        </MDBTooltip>
-                      </MDBListGroupItem>
-                      <MDBListGroupItem className="d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
-                        <div className="d-flex align-items-center">
-                          <MDBCheckbox
-                            name="flexCheck"
-                            value=""
-                            id="flexCheck"
-                            className="me-3"
-                          />
-                          Morbi leo risus
-                        </div>
-                        <MDBTooltip
-                          tag="a"
-                          wrapperProps={{ href: "#!" }}
-                          title="Remove item"
-                        >
-                          <MDBIcon fas icon="times" color="primary" />
-                        </MDBTooltip>
-                      </MDBListGroupItem>
-                    </MDBListGroup>
+
+                    <ChecklistForm />
+                    <TaskList tasks = {tasks} />
+
+
+
                   </MDBCardBody>
                 </MDBCard>
               </MDBCol>
